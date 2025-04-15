0e9be840d1db983648803712618787821b1b5bad
Roman <roma.abdula98@yahoo.com>
2025-04-15 23:20:05 +0500

    Add new class 'PossibleOperations', add operation, without logic this operation

---
+++ Abdulin-Bank/PossibleOperations.cs
@@ -0,0 +1,34 @@
+public class PossibleOperations
+{
+    public static void AddUser()
+    {
+        var user = new User
+    }
+
+    public static void ChangeUser(User user)
+    {
+
+    }
+
+    public static void DeleteUser(User user)
+    {
+
+    }
+
+    public static void AddBankAccount(BankAccount bankAccount)
+    {
+
+    }
+
+    public static void DeleteBankAccount(BankAccount bankAccount)
+    {
+
+    }
+
+    public static void CheckBankAccount (BankAccount bankAccount)
+    {
+
+    }
+
+    
+}
